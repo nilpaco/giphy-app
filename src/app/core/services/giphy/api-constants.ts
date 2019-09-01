9c6a80c5847f3a706fa1f2de2b6a7baa1f842327
nilpaco <nil@nuvolar.eu>
2019-09-02 00:56:01 +0200

    Add readme with live demo and change http to https

--- src/app/core/services/giphy/api-constants.ts
+++ src/app/core/services/giphy/api-constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://api.giphy.com/v1/gifs';
+export const BASE_URL = 'https://api.giphy.com/v1/gifs';
 
 export const SEARCH_URL = `${BASE_URL}/search`;
 export const TRENDING_URL = `${BASE_URL}/trending`;
